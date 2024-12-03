d91cab070895660f153936f8eed342708a45ea8e
Michael Deglin <michael@alignedup.com>
2024-12-03 19:04:59 +0200

    bug fixes

--- src/components/AddTreatmentDialog.tsx
+++ src/components/AddTreatmentDialog.tsx
@@ -1,18 +1,27 @@
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { useMutation } from "convex/react";
-import { useState } from "react";
-import { api } from "../../convex/_generated/api";
 import { Id } from "convex/_generated/dataModel";
-import { DatePicker } from "./ui/date-picker";
+import { useMutation } from "convex/react";
 import { he } from "date-fns/locale";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { api } from "../../convex/_generated/api";
+import { DatePicker } from "./ui/date-picker";
+
+interface FormInputs {
+  type: string;
+  description: string;
+  cost: string;
+  nextAppointment: string | undefined;
+  notes: string;
+}
 
 export function AddTreatmentDialog({
   patientId,
@@ -20,27 +29,38 @@ export function AddTreatmentDialog({
   patientId: Id<"patients">;
 }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [form, setForm] = useState({
-    type: "",
-    description: "",
-    cost: "",
-    nextAppointment: "",
-    notes: "",
-  });
-
   const addTreatment = useMutation(api.treatments.add);
 
-  const handleSubmit = async () => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<FormInputs>({
+    defaultValues: {
+      type: "",
+      description: "",
+      cost: "",
+      nextAppointment: "",
+      notes: "",
+    },
+    mode: "onBlur",
+  });
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
     await addTreatment({
       patientId,
-      cost: +form.cost,
+      cost: +data.cost,
       date: new Date().toISOString(),
-      description: form.description,
-      type: form.type,
-      notes: form.notes,
-      nextAppointment: form.nextAppointment,
+      description: data.description,
+      type: data.type,
+      notes: data.notes,
+      nextAppointment: data.nextAppointment || "",
     });
     setIsOpen(false);
+    reset();
   };
 
   return (
@@ -48,8 +68,11 @@ export function AddTreatmentDialog({
       <Button variant="secondary" onClick={() => setIsOpen(true)}>
         הוסף טיפול
       </Button>
-      <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <DialogContent className="max-w-lg px-12 pt-6 ">
+      <Dialog open={isOpen} onOpenChange={() => {
+        reset();
+        setIsOpen(!isOpen)
+      }}>
+        <DialogContent className="max-w-lg px-12 pt-6">
           <DialogHeader>
             <DialogTitle className="text-right text-xl p-2">
               הוסף טיפול
@@ -57,51 +80,60 @@ export function AddTreatmentDialog({
           </DialogHeader>
           <form
             className="space-y-4 flex flex-col gap-3"
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleSubmit();
-            }}
+            onSubmit={handleSubmit(onSubmit)}
           >
-            <Input
-              placeholder="סוג"
-              value={form.type}
-              onChange={(e) => setForm({ ...form, type: e.target.value })}
-              required
-            />
-            <Textarea
-              placeholder="תיאור"
-              value={form.description}
-              onChange={(e) =>
-                setForm({ ...form, description: e.target.value })
-              }
-            />
-            <Textarea
-              placeholder="הערות"
-              value={form.notes}
-              onChange={(e) => setForm({ ...form, notes: e.target.value })}
-            />
-            <Input
-              type="number"
-              placeholder="עלות"
-              value={form.cost}
-              onChange={(e) => setForm({ ...form, cost: e.target.value })}
-              required
-            />
+            <div className="space-y-2">
+              <Input
+                placeholder="סוג"
+                {...register("type", { required: "שדה חובה" })}
+                className={errors.type ? "border-red-500 shadow-sm" : ""}
+              />
+              {errors.type && (
+                <p className="text-sm text-red-600">{errors.type.message}</p>
+              )}
+            </div>
 
-            <DatePicker
-              fromDate={new Date()}
-              date={form.nextAppointment}
-              onDateChange={(date) => {
+            <Textarea placeholder="תיאור" {...register("description")} />
 
-                setForm({
-                  ...form,
-                  nextAppointment: date ? new Date(date).toISOString() : "",
-                });
-              }}
-              locale={he}
-              className={`w-full justify-start text-right h-10 p-2`}
-            />
-            
+            <Textarea placeholder="הערות" {...register("notes")} />
+
+            <div className="space-y-2">
+              <Input
+                type="number"
+                placeholder="עלות"
+                {...register("cost", {
+                  required: "שדה חובה",
+                  validate: (value) => parseFloat(value) > 0 || "עלות לא תקינה",
+                })}
+                className={errors.cost ? "border-red-500 shadow-sm" : ""}
+              />
+              {errors.cost && (
+                <p className="text-sm text-red-600">{errors.cost.message}</p>
+              )}
+            </div>
+
+            <div className="space-y-2">
+              <DatePicker
+                fromDate={new Date()}
+                date={watch("nextAppointment")}
+                onDateChange={(date) => {
+                  setValue(
+                    "nextAppointment",
+                    date ? new Date(date).toISOString() : undefined
+                  );
+                }}
+                locale={he}
+                className={`w-full justify-start text-right h-10 p-2 ${
+                  errors.nextAppointment ? "border-red-500 shadow-sm" : ""
+                }`}
+              />
+              {errors.nextAppointment && (
+                <p className="text-sm text-red-600">
+                  {errors.nextAppointment.message}
+                </p>
+              )}
+            </div>
+
             <Button type="submit" className="w-full">
               שמור
             </Button>
