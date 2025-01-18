ebd88e4bbdda643270a66e07cd3386cb924de08c
Michael Deglin <michael@alignedup.com>
2025-01-18 03:59:26 +0200

    maybe now wont show empty list on logout

--- src/components/Redirect/RootRedirect.tsx
+++ src/components/Redirect/RootRedirect.tsx
@@ -1,10 +1,11 @@
 import { useAuth } from "@clerk/clerk-react";
 import { Navigate } from "react-router-dom";
+import { LoadingScreen } from '../ui/LoadingScreen';
 
 const RootRedirect = () => {
-  const { userId } = useAuth();
+  const { userId, isLoaded } = useAuth();
   
-  // if(!isLoaded) return <LoadingScreen/>;
+  if(!isLoaded) return <LoadingScreen/>;
   
   if (userId) {
     return <Navigate to="/dashboard" replace />;
--- src/components/ui/LoadingScreen.tsx
+++ src/components/ui/LoadingScreen.tsx
@@ -2,8 +2,8 @@ import { Loader2 } from "lucide-react";
 
 export const LoadingScreen = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 backdrop-blur-sm z-50">
-      <Loader2 size={50} className='animate-spin'/>
+    <div className="bg-foreground fixed inset-0 flex items-center justify-center bg-opacity-50 backdrop-blur-sm z-50">
+      <Loader2 size={50} className='animate-spin text-secondary' />
     </div>
   );
 };
